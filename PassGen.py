bffe57cdfb5461c2ca76404c5f4a22271b9c9bdf
jerrbear1011 <jaymon1011@yahoo.com>
2026-09-04 14:29:50 -0400

    created genpass.py
    Created password Gen program,

---
+++ PassGen.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+msg = 'Welcome to PassGen'
+passWord = ''
+temp = ''
+
+print(msg)
+
+print('How many characters do you need your password to be?')
+
+num_chars = int(input())
+
+print('Does your password need to contain special characters? (y/n)')
+
+special_chars = input().lower()
+
+if special_chars == 'y':
+    for i in range(num_chars):
+        temp = random.choice(string.ascii_letters + string.digits + string.punctuation)
+        passWord = passWord+temp
+    print(passWord)
+
+elif special_chars == 'n':
+    for i in range(num_chars):
+        temp = random.choice(string.ascii_letters + string.digits)
+        passWord = passWord+temp
+    print(passWord)
