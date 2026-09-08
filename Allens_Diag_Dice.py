6310710e1c7bc9c56c0a590cc40a5566245c1ff1
jerrbear1011 <jaymon1011@yahoo.com>
2026-09-08 11:10:25 -0400

    Created Allens diag program

---
+++ Allens_Diag_Dice.py
@@ -0,0 +1,16 @@
+
+DTC_Dice_list = ['P9999 - Engine on fire','P0FUK - Technician Required','P0420 - Catalytic Converter Efficiency Below Threshold',
+              'U2DUM - CAN Bus is being stupid','C1374 - Wheel doing wheel things','U0420 - Received invalid data from some dude named Jimmy',
+              'P1602 - Vehicle has become sentient','Try again']
+
+Diag_Dice_List = ['Car is haunted','Probably the alternator','Needs Premium Air','Has Temu Parts','Bad Ground','Electrical, good luck Allen',
+                  'Engine Appers to be made of Engine','Loose connector','Turn Signal is on','Tech is hungover','Vacuum Leak',
+                  'Vehicle is shy','Probably the Alternator','Probably NOT the Alternator','Previous Technician','Service writer just wants a commission',
+                  'Service writer needs to pay off cruise','Customer wants a loaner']
+
+Recommendation_Work_Dice_List = ['Replace entire engine','Consult Mechanic','Perform ECU relearn',
+                                 'Do not investigate further','Sacrifice a chicken','Disconnect battery for 10 minutes',
+                                 'Replace the ECM module, errr I mean the PCM, Maybe the BCM? honestly, who knows.','Contact priest',
+                                    'put in 2 week notice', 'Clear code']
+
+
