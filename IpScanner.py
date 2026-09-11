a9d7f2f6057610d0546fba35e4fe59da5c454a86
jerrbear1011 <jaymon1011@yahoo.com>
2026-09-11 16:40:11 -0400

    added ai code to help learn API coding.

--- IpScanner.py
+++ IpScanner.py
@@ -4,12 +4,21 @@ ListOfIp = []
 Network = input("Enter the network to scan (e.g., 192.168.1): ")
 IP_Start = input("Enter first IP to scan:")
 IP_End= input("Enter last IP to scan:")
+attempts = input("Enter number of attempts to ping each IP: ")
 
-def get_ip(IPad,IPs, IPe):
-    IPs, IPe, IPad = int(IPs), int(IPe), str(IPad)
-    address = IPad + str(IPs)
-    print(ping(address, 4, 1, 2, None, None))
-
+def get_ip(IPad,IPs, IPe, attempts):
+    IPs, IPe, IPad, attempts = int(IPs), int(IPe), str(IPad), int(attempts)
 
 
-get_ip(Network, IP_Start, IP_End)
+    for IPs in range(IPs, IPe + 1):
+        address = IPad + str(IPs)
+        new = ""
+        new = str(ping(address, attempts, 1, 2, None, None))
+        #print(new)
+        return ListOfIp.append(new)
+
+
+
+get_ip(Network, IP_Start, IP_End, attempts)
+
+print(ListOfIp)
