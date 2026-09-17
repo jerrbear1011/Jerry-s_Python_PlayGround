fa8db6b29ccfdd5360b5c5a5931565c74002f162
jerrbear1011 <jaymon1011@yahoo.com>
2026-09-17 11:45:38 -0400

    Added Custom entries and save fuctions

--- Allens_Diag_Dice/Allens_Diag_Dice.py
+++ Allens_Diag_Dice/Allens_Diag_Dice.py
@@ -84,9 +84,95 @@ def ResetToFactory():
     LoadDefaultItems()
 
 
+def save_dtc(new_dtc, dtc_listbox, add_window):
+    Directory = Path(__file__).resolve().parent
+    DTC_Dice_list_path = Directory / 'CustomItems' / 'DTC_Dice_list_Custom.txt'
+    if new_dtc.strip():
+        DTC_Dice_list_custom.append(new_dtc.strip())
+        dtc_listbox.insert(tk.END, new_dtc.strip())
+        add_window.destroy()
+        with open(DTC_Dice_list_path, 'a') as file:
+            file.write(new_dtc.strip()+ "\n")
+    DTC_Dice_list.clear()
+    Add_Custom_Items()
+
+def save_diag(new_diag, diag_listbox, add_window):
+    Directory = Path(__file__).resolve().parent
+    Diag_Dice_List_path = Directory / 'CustomItems' / 'Diag_Dice_List_Custom.txt'
+    if new_diag.strip():
+        Diag_Dice_List_Custom.append(new_diag.strip())
+        diag_listbox.insert(tk.END, new_diag.strip())
+        add_window.destroy()
+        with open(Diag_Dice_List_path, 'a') as file:
+            file.write(new_diag.strip() + "\n")
+    Diag_Dice_List_Custom.clear()
+    Add_Custom_Items()
+
+def save_Rec(new_rec, diag_listbox, add_window):
+    Directory = Path(__file__).resolve().parent
+    Recommendation_Work_Dice_List_path = Directory / 'CustomItems' / 'Recom_Work_Dice_List_Custom.txt'
+    if new_rec.strip():
+        Recommendation_Work_Dice_List_Custom.append(new_rec.strip())
+        diag_listbox.insert(tk.END, new_rec.strip())
+        add_window.destroy()
+        with open(Recommendation_Work_Dice_List_path, 'a') as file:
+            file.write(new_rec.strip() + "\n")
+    Recommendation_Work_Dice_List_Custom .clear()
+    Add_Custom_Items()
+
+def ButtonTest():
+    print("click")
+
+
 LoadDefaultItems()  
 Add_Custom_Items() 
 #ResetToFactory() ## testing remove later
+
+
+def add_dtc(config_window, dtc_listbox):
+    add_window = tk.Toplevel(config_window)
+    add_window.title("Add DTC")
+    add_window.geometry("400x150")
+    tk.Label(add_window, text="Enter new DTC:").pack(pady =10)
+
+    dtc_entry = tk.Entry(add_window, width=45)
+    dtc_entry.pack()
+
+    tk.Button(
+        add_window,
+        text='save',
+        command=lambda: save_dtc(dtc_entry.get(), dtc_listbox, add_window)
+    ).pack(pady=10)
+
+def add_diag(config_window, diag_listbox):
+    add_window = tk.Toplevel(config_window)
+    add_window.title("Add Diag")
+    add_window.geometry("400x150")
+    tk.Label(add_window, text="Enter new Diag:").pack(pady =10)
+
+    diag_entry = tk.Entry(add_window, width=45)
+    diag_entry.pack()
+
+    tk.Button(
+        add_window,
+        text='save',
+        command=lambda: save_diag(diag_entry.get(), diag_listbox, add_window)
+    ).pack(pady=10)
+
+def add_rec(config_window, Rec_listbox):
+    add_window = tk.Toplevel(config_window)
+    add_window.title("Add Recommendation")
+    add_window.geometry("400x150")
+    tk.Label(add_window, text="Enter new recommendation:").pack(pady =10)
+
+    diag_entry = tk.Entry(add_window, width=45)
+    diag_entry.pack()
+
+    tk.Button(
+        add_window,
+        text='save',
+        command=lambda: save_Rec(diag_entry.get(), Rec_listbox, add_window)
+    ).pack(pady=10)
 
 def openConfig():
     config_window = tk.Toplevel(window)
@@ -102,25 +188,24 @@ def openConfig():
     dtc_listbox = tk.Listbox(
         dtc_frame
     )
-    dct_listbox = tk.Listbox(
-        dtc_frame
-    )
-    dct_listbox.pack(fill="both", expand=True)
+    dtc_listbox.pack(fill="both", expand=True)
     dtc_frame.pack(fill="both", expand=True, padx=10, pady=10)
     for item in DTC_Dice_list_custom:
-        dct_listbox.insert(tk.END, item)
+        dtc_listbox.insert(tk.END, item)
 
 
     DTC_button_frame = tk.Frame(dtc_frame)
     DTC_button_frame.pack()
 
-    DTC_add_button = tk.Button(DTC_button_frame, text="Add",command=ResetToFactory)
+    DTC_add_button = tk.Button(DTC_button_frame,
+                                text="Add",
+                                command=lambda: add_dtc(config_window, dtc_listbox))
     DTC_add_button.pack(side="left")
 
-    DTC_edit_button = tk.Button(DTC_button_frame, text="Edit")
+    DTC_edit_button = tk.Button(DTC_button_frame, text="Edit", command=ButtonTest)
     DTC_edit_button.pack(side="left")
 
-    DTC_delete_button = tk.Button(DTC_button_frame, text="Delete")
+    DTC_delete_button = tk.Button(DTC_button_frame, text="Delete", command=ButtonTest)
     DTC_delete_button.pack(side="left")
 
 
@@ -149,9 +234,6 @@ def openConfig():
     Diag_listbox = tk.Listbox(
         Diag_frame
     )
-    Diag_listbox = tk.Listbox(
-        Diag_frame
-    )
     Diag_listbox.pack(fill="both", expand=True)
     Diag_frame.pack(fill="both", expand=True, padx=10, pady=10)
     for item in Diag_Dice_List_Custom:
@@ -160,13 +242,13 @@ def openConfig():
     Diag_button_frame = tk.Frame(Diag_frame)
     Diag_button_frame.pack()
 
-    Diag_add_button = tk.Button(Diag_button_frame, text="Add")
+    Diag_add_button = tk.Button(Diag_button_frame, text="Add", command=lambda: add_diag(config_window, Diag_listbox))
     Diag_add_button.pack(side="left")
 
-    Diag_edit_button = tk.Button(Diag_button_frame, text="Edit")
+    Diag_edit_button = tk.Button(Diag_button_frame, text="Edit", command=ButtonTest)
     Diag_edit_button.pack(side="left")
 
-    Diag_delete_button = tk.Button(Diag_button_frame, text="Delete")
+    Diag_delete_button = tk.Button(Diag_button_frame, text="Delete", command=ButtonTest)
     Diag_delete_button.pack(side="left")
 
 
@@ -195,9 +277,7 @@ def openConfig():
     Rec_listbox = tk.Listbox(
         Rec_frame
     )
-    Rec_listbox = tk.Listbox(
-        Rec_frame
-    )
+
     Rec_listbox.pack(fill="both", expand=True)
     Rec_frame.pack(fill="both", expand=True, padx=10, pady=10)
     for item in Recommendation_Work_Dice_List_Custom:
@@ -208,13 +288,13 @@ def openConfig():
     rec_button_frame = tk.Frame(Rec_frame)
     rec_button_frame.pack()
 
-    rec_add_button = tk.Button(rec_button_frame, text="Add")
+    rec_add_button = tk.Button(rec_button_frame, text="Add", command=lambda: add_rec(config_window, Rec_listbox))
     rec_add_button.pack(side="left")
 
-    rec_edit_button = tk.Button(rec_button_frame, text="Edit")
+    rec_edit_button = tk.Button(rec_button_frame, text="Edit", command=ButtonTest)
     rec_edit_button.pack(side="left")
 
-    rec_delete_button = tk.Button(rec_button_frame, text="Delete")
+    rec_delete_button = tk.Button(rec_button_frame, text="Delete", command=ButtonTest)
     rec_delete_button.pack(side="left")
     
 
@@ -333,4 +413,6 @@ config_button = tk.Button(
     command=openConfig
 )
 config_button.pack()
+
+
 window.mainloop()
